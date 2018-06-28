f357b28ec816da3bf1fcfe9bab168bb1021a2216
bill.claytor <bill.claytor@puppet.com>
2018-06-28 12:24:29 -0700

    (SLV-127) Updated url_helper and spec to provide example

--- lib/ref_arch_setup/url_helper.rb
+++ lib/ref_arch_setup/url_helper.rb
@@ -2,16 +2,53 @@
 module RefArchSetup
   # Code to illustrate a Beaker-based method of deriving a tarball URL
   class UrlHelper
+    # Determine is a given URL is accessible
+    # *** copied from Beaker to illustrate the example ***
+    #
+    # @param [String] link The URL to examine
+    # @return [Boolean] true if the URL has a '200' HTTP response code, false otherwise
+    # @example
+    #  extension = link_exists?("#{URL}.tar.gz") ? ".tar.gz" : ".tar"
+    #
+    def link_exists?(link)
+      require "net/http"
+      require "net/https"
+      require "open-uri"
+      url = URI.parse(link)
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = (url.scheme == 'https')
+      http.verify_mode = (OpenSSL::SSL::VERIFY_NONE)
+      http.start do |http|
+        return http.head(url.request_uri).code == "200"
+      end
+    end
+
+    # Prepares the host by setting the host["dist"]
+    #
     # Extracted from pe_utils.prepare_hosts
     #   this version leaves filename = "#{host['dist']}" to show how it was originally used
+    #
+    # @param [Host] host The unix style host where PE will be installed
+    # @return [String] the host["dist"] aka PE filename
+    # @example
+    #   prepare_ras_host(host)
+    #
     def prepare_ras_host(host)
       # host['dist'] is used for the filename
-      host["dist"] = "puppet-enterprise-#{host['pe_ver']}-#{host['platform']}"
+      host["dist"] = "puppet-enterprise-#{host["pe_ver"]}-#{host["platform"]}"
     end
 
     # Extracted from pe_utils.fetch_pe_on_unix
-    #   this assumes being called by beaker with a host (master)
-    def get_ras_pe_url(host)
+    #   assumes being called by beaker with a host (master)
+    #   this version calls prepare_ras_host which sets host["dist"];
+    #   similar to beaker implementation
+    #
+    # @param [Host] host The unix style host where PE will be installed
+    # @return [String] the tarball URL
+    # @example
+    #   url = get_ras_pe_tarball_url(host)
+    #
+    def get_ras_pe_tarball_url(host)
       prepare_ras_host(host)
       path = host["pe_dir"]
       filename = host["dist"]
@@ -24,5 +61,28 @@ module RefArchSetup
 
       return url
     end
+
+    # Extracted from pe_utils.fetch_pe_on_unix
+    #   assumes being called by beaker with a host (master)
+    #   this version is simplified by removing the call to prepare_ras_host
+    #
+    # @param [Host] host The unix style host where PE will be installed
+    # @return [String] the tarball URL
+    # @example
+    #   url = get_ras_pe_tarball_url(host)
+    #
+    def get_ras_pe_tarball_url_simple(host)
+      path = host["pe_dir"]
+      filename = "puppet-enterprise-#{host["pe_ver"]}-#{host["platform"]}"
+      extension = ".tar.gz"
+      url = "#{path}/#{filename}#{extension}"
+
+      unless link_exists?(url)
+        raise "attempting to construct download URL for #{host}, #{url} does not exist"
+      end
+
+      return url
+    end
+
   end
 end
--- spec/ref_arch_setup/url_helper_spec.rb
+++ spec/ref_arch_setup/url_helper_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require_relative "../../lib/ref_arch_setup/url_helper.rb"
 
 describe RefArchSetup::UrlHelper do
 
@@ -6,17 +7,44 @@ describe RefArchSetup::UrlHelper do
 
   TEST_PE_DIR = "http://enterprise.delivery.puppetlabs.net/archives/internal/2018.1".freeze
   TEST_PE_VER = "2018.1.0-rc14".freeze
-  TEST_HOST = "el-6-x86_64".freeze
+  TEST_PLATFORM = "el-6-x86_64".freeze
+  TEST_DIST = "puppet-enterprise-2018.1.0-rc14-el-6-x86_64".freeze
   TEST_EXPECTED_URL = "http://enterprise.delivery.puppetlabs.net/archives/internal/2018.1/puppet-enterprise-2018.1.0-rc14-el-6-x86_64.tar.gz"
 
-  describe "#get_url" do
+  let!(:host) {{"pe_dir" => TEST_PE_DIR, "pe_ver" => TEST_PE_VER, "platform" => TEST_PLATFORM}}
 
-    context "when given a pe_dir, pe_ver, and host" do
+  describe "#prepare_ras_host" do
+
+    context "when given a host with a pe_dir, pe_ver, and platform" do
+
+      it "it sets and returns the filename as host['dist']" do
+        expect(host["dist"]).to eq(nil)
+        expect(url_helper.prepare_ras_host(host)).to eq(TEST_DIST)
+        expect(host["dist"]).to eq(TEST_DIST)
+      end
+
+    end
+
+  end
+
+  describe "#get_ras_pe_tarball_url" do
+
+    context "when given a host with a pe_dir, pe_ver, and platform" do
 
       it "it returns the download URL" do
+        expect(url_helper.get_ras_pe_tarball_url(host)).to eq(TEST_EXPECTED_URL)
+      end
 
-        expect(url_helper.get_url(TEST_PE_DIR, TEST_PE_VER, TEST_HOST)).to eq(TEST_EXPECTED_URL)
+    end
 
+  end
+
+  describe "#get_ras_pe_tarball_url_simple" do
+
+    context "when given a host with a pe_dir, pe_ver, and platform" do
+
+      it "it returns the download URL" do
+        expect(url_helper.get_ras_pe_tarball_url_simple(host)).to eq(TEST_EXPECTED_URL)
       end
 
     end
