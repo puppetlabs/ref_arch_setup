b0a18b07316a128c35d505585c94847baa4d45a0
bill.claytor <bill.claytor@puppet.com>
2018-06-29 11:24:39 -0700

    (SLV-127) Updated spec to fix url mismatch

--- spec/ref_arch_setup/url_helper_spec.rb
+++ spec/ref_arch_setup/url_helper_spec.rb
@@ -4,7 +4,7 @@ require_relative "../../lib/ref_arch_setup/url_helper.rb"
 describe RefArchSetup::UrlHelper do
   let(:url_helper) { RefArchSetup::UrlHelper.new }
 
-  TEST_PE_DIR = "http://enterprise.delivery.puppetlabs.net/archives/internal/2018.1".freeze
+  TEST_PE_DIR = "http://test.net/2018.1".freeze
   TEST_PE_VER = "2018.1.0-rc14".freeze
   TEST_PLATFORM = "el-6-x86_64".freeze
   TEST_DIST = "puppet-enterprise-2018.1.0-rc14-el-6-x86_64".freeze
