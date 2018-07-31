560940833b26c33f811d28043c6c39e6d9c5fb4c
bill.claytor <bill.claytor@puppet.com>
2018-07-31 15:46:39 -0700

    (SLV-136) Removed outdated files

--- acceptance/tests/help_test.rb
+++
@@ -1,9 +0,0 @@
-test_name "output help text" do
-  step "Run with --help" do
-    exe = "#{__dir__}/../../bin/ref_arch_setup --help"
-    puts "it is #{exe}"
-    foo = `#{exe}`
-    puts "output is:"
-    puts foo
-  end
-end
--- acceptance/tests/nothing.rb
+++
@@ -1,3 +0,0 @@
-test_name "Setup nothing" do
-  puts "nothing to do here, move along"
-end
