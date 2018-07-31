064a012a35fd3de6dd23be368e983f1e8e0f3005
bill.claytor <bill.claytor@puppet.com>
2018-07-31 15:52:15 -0700

    (SLV-136) Uncommented new acceptance tests in options.rb

--- acceptance/config/options.rb
+++ acceptance/config/options.rb
@@ -19,9 +19,9 @@
     "acceptance/pre_suites/40_download_pe_tarball.rb"
   ],
   :tests => [
-    "acceptance/tests/10_install_test_remote_master_tarball_url.rb"
-    # "acceptance/tests/20_install_test_remote_master_tarball_path_controller.rb",
-    # "acceptance/tests/30_install_test_remote_master_tarball_path_master.rb"
+    "acceptance/tests/10_install_test_remote_master_tarball_url.rb",
+    "acceptance/tests/20_install_test_remote_master_tarball_path_controller.rb",
+    "acceptance/tests/30_install_test_remote_master_tarball_path_master.rb"
   ],
   "is_puppetserver"            => true,
   "use-service"                => true, # use service scripts to start/stop stuff
