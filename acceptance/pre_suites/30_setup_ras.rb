f19d156aa6dda98cd020040cfbfd876e57e6463b
bill.claytor <bill.claytor@puppet.com>
2018-08-07 15:54:33 -0700

    (SLV-136) Updated ras setup to specify controller only

--- acceptance/pre_suites/30_setup_ras.rb
+++ acceptance/pre_suites/30_setup_ras.rb
@@ -1,16 +1,14 @@
 test_name "install RAS on controller" do
   step "copy RAS gem" do
-    hosts = [controller]
-    scp_to(hosts, "#{__dir__}/../../pkg", "ref_arch_setup")
+    scp_to(controller, "#{__dir__}/../../pkg", "ref_arch_setup")
   end
 
   step "install RAS on controller" do
-    hosts = [controller]
     version = RefArchSetup::Version::STRING
     gem = "ref_arch_setup-#{version}.gem"
     command = "gem install #{RAS_PATH}/#{gem}"
 
-    on hosts, command
+    on controller, command
   end
 
   step "copy fixtures and modules" do
