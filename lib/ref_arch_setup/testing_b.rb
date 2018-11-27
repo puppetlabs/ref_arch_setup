0f7e9a8172ff39359ecbff3faeb3f26de1910205
bill.claytor <bill.claytor@puppet.com>
2018-11-27 12:19:28 -0800

    (SLV-236) Adding undocumented files to test pipeline; will remove

---
+++ lib/ref_arch_setup/testing_b.rb
@@ -0,0 +1,35 @@
+module RefArchSetup
+  # Module for holding version info
+  module TestingB
+    # Version number
+    STRING = "0.5.1".freeze
+
+    def a
+      puts "hello"
+    end
+
+    def b
+      puts "hello"
+    end
+
+    def c
+      puts "hello"
+    end
+
+    def d
+      puts "hello"
+    end
+
+    def e
+      puts "hello"
+    end
+
+    def f
+      puts "hello"
+    end
+
+    def g
+      puts "hello"
+    end
+  end
+end
